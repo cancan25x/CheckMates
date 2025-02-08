e2a1625b50dcb5090c8b6871e897b9e244327bca
Nate Hartung <105759521+Tungy79@users.noreply.github.com>
2025-02-08 03:38:14 -0700

    Don't be mad, but I created this
    This is a working checklist model. It is dynamically sized and can add or remove items and clear the whole list. I just threw the entire program in here because I don't know what you need to make it work on your systems. 
    
    Also, I quickly made a logo on MS Paint that can definitely be updated.....

---
+++ IT488_CheckMates_Checklist/IT488_CheckMates_Checklist/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace IT488_CheckMates_Checklist
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.itemTextBox = new System.Windows.Forms.TextBox();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(15, 15, 3, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(477, 135);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Check It Off!";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedListBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(30, 141);
+            this.checkedListBox1.MaximumSize = new System.Drawing.Size(400, 276);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(400, 276);
+            this.checkedListBox1.TabIndex = 9;
+            // 
+            // addButton
+            // 
+            this.addButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.addButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.addButton.Location = new System.Drawing.Point(30, 440);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(142, 36);
+            this.addButton.TabIndex = 10;
+            this.addButton.Text = "Add To List";
+            this.addButton.UseVisualStyleBackColor = false;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // itemTextBox
+            // 
+            this.itemTextBox.Location = new System.Drawing.Point(194, 447);
+            this.itemTextBox.Name = "itemTextBox";
+            this.itemTextBox.Size = new System.Drawing.Size(236, 22);
+            this.itemTextBox.TabIndex = 11;
+            // 
+            // removeButton
+            // 
+            this.removeButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.removeButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.removeButton.Location = new System.Drawing.Point(30, 502);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(142, 36);
+            this.removeButton.TabIndex = 12;
+            this.removeButton.Text = "Remove From List";
+            this.removeButton.UseVisualStyleBackColor = false;
+            // 
+            // clearButton
+            // 
+            this.clearButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.clearButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.clearButton.Location = new System.Drawing.Point(194, 502);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(236, 36);
+            this.clearButton.TabIndex = 13;
+            this.clearButton.Text = "Clear List";
+            this.clearButton.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(477, 568);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.itemTextBox);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox itemTextBox;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ IT488_CheckMates_Checklist/IT488_CheckMates_Checklist/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace IT488_CheckMates_Checklist
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            removeButton.Click += new EventHandler(RemoveButton_Click);
+
+            clearButton.Click += new EventHandler(ClearButton_Click);
+
+            itemTextBox.KeyDown += new KeyEventHandler(ItemTextBox_KeyDown);
+            
+            this.ResizeBegin += new EventHandler(Form1_Resize);
+
+        }
+
+        private void RemoveButton_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.SelectedItem != null)
+            {
+                checkedListBox1.Items.Remove(checkedListBox1.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Please select an item to remove.");
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Clear();
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            checkedListBox1.Width = this.ClientSize.Width;
+            checkedListBox1.Height = this.ClientSize.Height;
+        }
+
+        private void ItemTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                addButton.PerformClick();
+
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string newItem = itemTextBox.Text.Trim();
+
+            if (!string.IsNullOrEmpty(newItem))
+            {
+                checkedListBox1.Items.Add(newItem);
+
+                itemTextBox.Text = string.Empty;
+            }
+            else
+            {
+                MessageBox.Show("Please enter an item to add.");
+            }
+        }
+
+
+    }
+}
