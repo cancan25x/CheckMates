c61ecbdfe3491e79d1edb1f973c021539c02a136
Canyon R <Leggomyfneggo@gmail.com>
2025-02-17 16:16:03 -0500

    SQL Addition to project
    Added the SQL code and database to project

---
+++ IT488_CheckMates_Homescreen/IT488_CheckMates_Homescreen/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace IT488_CheckMates_Homescreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBoxList = new System.Windows.Forms.ComboBox();
+            this.comboBoxCat = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(44, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(303, 138);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(311, 179);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(481, 179);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // comboBoxList
+            // 
+            this.comboBoxList.FormattingEnabled = true;
+            this.comboBoxList.Location = new System.Drawing.Point(441, 96);
+            this.comboBoxList.Name = "comboBoxList";
+            this.comboBoxList.Size = new System.Drawing.Size(181, 21);
+            this.comboBoxList.TabIndex = 4;
+            this.comboBoxList.SelectedIndexChanged += new System.EventHandler(this.comboBoxList_SelectedIndexChanged);
+            // 
+            // comboBoxCat
+            // 
+            this.comboBoxCat.FormattingEnabled = true;
+            this.comboBoxCat.Location = new System.Drawing.Point(441, 40);
+            this.comboBoxCat.Name = "comboBoxCat";
+            this.comboBoxCat.Size = new System.Drawing.Size(182, 21);
+            this.comboBoxCat.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(438, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Organize By:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(438, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Select a List:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnF2;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dataGridView1.Location = new System.Drawing.Point(44, 239);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ShowCellToolTips = false;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.Size = new System.Drawing.Size(606, 218);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(686, 491);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxCat);
+            this.Controls.Add(this.comboBoxList);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "CheckItOff";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBoxList;
+        private System.Windows.Forms.ComboBox comboBoxCat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ IT488_CheckMates_Homescreen/IT488_CheckMates_Homescreen/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace IT488_CheckMates_Homescreen
+{
+    public partial class Form1 : Form
+    {
+        SQLiteConnection connectionString = new SQLiteConnection(@"Data Source = ..\..\Files\toDoList.db; Version=3;");
+        //BindingList<CheckoffList> checkoffLists = new BindingList<CheckoffList>();
+        int num = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            //SetupDataGridView();
+            fillComboBox();
+            fillComboxCat();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            comboBoxCat.SelectedIndex = 0;
+           
+        }
+
+        private void fill_grid()
+        {
+
+            dataGridView1.Refresh();
+
+            string listName = comboBoxList.SelectedItem.ToString();
+            //string prior = comboBoxCat.SelectedItem.ToString();
+            connectionString.Open();
+
+            //the query command to pull the data
+            SQLiteCommand cmd = new SQLiteCommand($@"SELECT taskName AS ""Name"", dueDate AS ""Due Date"", priority AS ""Priority"" FROM {listName};", connectionString);
+
+            //data to adapter the data in the grid
+            DataTable dt = new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+
+            DataGridViewButtonColumn deleteButtonColumn = new DataGridViewButtonColumn();
+            deleteButtonColumn.HeaderText = "Delete";
+            deleteButtonColumn.Name = "Delete";
+            deleteButtonColumn.Text = "Delete";
+            deleteButtonColumn.UseColumnTextForButtonValue = true;
+            int columnIndexs = 0;
+            if (dataGridView1.Columns["Delete"] == null)
+            {
+                dataGridView1.Columns.Insert(columnIndexs, deleteButtonColumn);
+            }
+           
+            DataGridViewButtonColumn editButtonColumn = new DataGridViewButtonColumn();
+            editButtonColumn.HeaderText = "Edit";
+            editButtonColumn.Name = "Edit";
+            editButtonColumn.Text = "Edit";
+            editButtonColumn.UseColumnTextForButtonValue = true;
+            int columnIndexss = 1;
+            if (dataGridView1.Columns["Edit"] == null)
+            {
+                dataGridView1.Columns.Insert(columnIndexss, editButtonColumn);
+            }
+          
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            connectionString.Close();
+
+            DataGridViewCheckBoxColumn checkColumn = new DataGridViewCheckBoxColumn();
+            checkColumn.Name = "X";
+            checkColumn.HeaderText = "X";
+            checkColumn.Width = 50;
+            checkColumn.ReadOnly = false;
+            checkColumn.FillWeight = 10;
+            while (num < 2)
+            {
+                dataGridView1.Columns.Add(checkColumn);
+                num = num + num;
+            }
+        }
+
+        public void fillComboBox()
+        {
+            connectionString.Open();
+            comboBoxList.Items.Clear();
+            SQLiteCommand cmd = new SQLiteCommand(@"SELECT NAME FROM sqlite_master WHERE TYPE =""table"" ORDER BY NAME;", connectionString);
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+            adapter.Fill(dt);
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                comboBoxList.Items.Add(dr["Name"].ToString());
+            }
+            connectionString.Close();
+        }
+
+        private void comboBoxList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            fill_grid();
+        }
+
+        public void fillComboxCat()
+        {
+            comboBoxCat.Items.Add("High");
+            comboBoxCat.Items.Add("Medium");
+            comboBoxCat.Items.Add("Low");
+            comboBoxCat.SelectedItem = "High";
+            comboBoxCat.SelectedText = "High";
+        }
+
+        /*
+        private void SetupDataGridView()
+        {
+            dataGridView1.AutoGenerateColumns = false;
+
+            DataGridViewColumn nameColumn = new DataGridViewTextBoxColumn();
+            nameColumn.DataPropertyName = "Name";
+            nameColumn.HeaderText = "List Name";
+            dataGridView1.Columns.Add(nameColumn);
+
+            DataGridViewColumn dueDateColumn = new DataGridViewTextBoxColumn();
+            dueDateColumn.DataPropertyName = "DueDate";
+            dueDateColumn.HeaderText = "Due Date";
+            dataGridView1.Columns.Add(dueDateColumn);
+
+            DataGridViewColumn priorityColumn = new DataGridViewTextBoxColumn();
+            priorityColumn.DataPropertyName = "Priority";
+            priorityColumn.HeaderText = "Priority";
+            dataGridView1.Columns.Add(priorityColumn);
+
+            dataGridView1.DataSource = checkoffLists;
+        }       
+        
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                checkoffLists.RemoveAt(dataGridView1.SelectedRows[0].Index);
+            }
+        }
+        
+        private void btnCreateList_Click(object sender, EventArgs e)
+        {
+            ListCreationForm listCreationForm = new ListCreationForm();
+            if (listCreationForm.ShowDialog() == DialogResult.OK)
+            {
+                CheckoffList newList = listCreationForm.GetCheckoffList();
+                checkoffLists.Add(newList);
+            }
+        }
+        */
+
+    }
+}
+
