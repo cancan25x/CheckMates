12ccbaa6c4f28b259a0a668694dbe8920dabba6c
Canyon R <Leggomyfneggo@gmail.com>
2025-02-23 04:11:46 -0500

    Combined Projects
    Had to comment out code for firework display. I do not know how to embed pictures into projects yet.

---
+++ IT488_CheckMates_CombinedList/IT488_CheckMates_Checklist/HomePage.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace IT488_CheckMates_Checklist
+{
+    partial class HomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomePage));
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.itemTextBox = new System.Windows.Forms.TextBox();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.TextBox();
+            this.editButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(11, 12, 2, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(359, 110);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Check It Off!";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedListBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(23, 124);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.checkedListBox1.MaximumSize = new System.Drawing.Size(301, 225);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(301, 214);
+            this.checkedListBox1.TabIndex = 9;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // addButton
+            // 
+            this.addButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.addButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.addButton.Location = new System.Drawing.Point(23, 354);
+            this.addButton.Margin = new System.Windows.Forms.Padding(2);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(106, 29);
+            this.addButton.TabIndex = 10;
+            this.addButton.Text = "Add a new list";
+            this.addButton.UseVisualStyleBackColor = false;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // itemTextBox
+            // 
+            this.itemTextBox.Location = new System.Drawing.Point(147, 363);
+            this.itemTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.itemTextBox.Name = "itemTextBox";
+            this.itemTextBox.Size = new System.Drawing.Size(177, 20);
+            this.itemTextBox.TabIndex = 11;
+            // 
+            // removeButton
+            // 
+            this.removeButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.removeButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.removeButton.Location = new System.Drawing.Point(23, 445);
+            this.removeButton.Margin = new System.Windows.Forms.Padding(2);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(106, 29);
+            this.removeButton.TabIndex = 12;
+            this.removeButton.Text = "Remove From List";
+            this.removeButton.UseVisualStyleBackColor = false;
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClearButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ClearButton.Location = new System.Drawing.Point(147, 399);
+            this.ClearButton.Margin = new System.Windows.Forms.Padding(2);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(177, 29);
+            this.ClearButton.TabIndex = 13;
+            this.ClearButton.Text = "Load List";
+            this.ClearButton.UseVisualStyleBackColor = false;
+            // 
+            // listBox
+            // 
+            this.listBox.Location = new System.Drawing.Point(168, 39);
+            this.listBox.Name = "listBox";
+            this.listBox.ReadOnly = true;
+            this.listBox.Size = new System.Drawing.Size(178, 20);
+            this.listBox.TabIndex = 14;
+            this.listBox.Visible = false;
+            // 
+            // editButton
+            // 
+            this.editButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.editButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.editButton.Location = new System.Drawing.Point(23, 399);
+            this.editButton.Margin = new System.Windows.Forms.Padding(2);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(106, 29);
+            this.editButton.TabIndex = 15;
+            this.editButton.Text = "Edit List Name";
+            this.editButton.UseVisualStyleBackColor = false;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // HomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(359, 485);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.itemTextBox);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "HomePage";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.HomePage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox itemTextBox;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.TextBox listBox;
+        private System.Windows.Forms.Button editButton;
+    }
+}
+
---
+++ IT488_CheckMates_CombinedList/IT488_CheckMates_Checklist/TaskPage.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using IT488_CheckMates_Checklist;
+
+namespace IT488_CheckMates_Homescreen
+{
+    public partial class TaskPage : Form
+    {
+        public static TaskPage instance;
+
+        SQLiteConnection connectionString = new SQLiteConnection(@"Data Source = ..\..\Files\toDoList.db; Version=3;");
+        int num = 1;
+        public TaskPage()
+        {
+            InitializeComponent();
+            instance = this;      
+            fill_grid();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        //Fills the grid on task page by taking the list name from the homepage this is to only be used when loading in
+        public void fill_grid()
+        {
+            try
+            {
+                string listName = HomePage.instance.listName.Text;
+                dataGridView1.Refresh();
+                connectionString.Open();
+                SQLiteCommand cmd = new SQLiteCommand($@"SELECT taskName AS ""Name"", dueDate AS ""Due Date"", priority AS ""Priority"" FROM {listName};", connectionString);
+                DataTable dt = new DataTable();
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+                /*  Incase we want a delet button in the grid
+                DataGridViewButtonColumn deleteButtonColumn = new DataGridViewButtonColumn();
+                deleteButtonColumn.HeaderText = "Delete";
+                deleteButtonColumn.Name = "Delete";
+                deleteButtonColumn.Text = "Delete";
+                deleteButtonColumn.UseColumnTextForButtonValue = true;
+                int columnIndexs = 0;
+                if (dataGridView1.Columns["Delete"] == null)
+                {
+                    dataGridView1.Columns.Insert(columnIndexs, deleteButtonColumn);
+                } 
+                */
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+                connectionString.Close();
+
+                DataGridViewCheckBoxColumn checkColumn = new DataGridViewCheckBoxColumn();
+                checkColumn.Name = "X";
+                checkColumn.HeaderText = "X";
+                checkColumn.Width = 50;
+                checkColumn.ReadOnly = false;
+                checkColumn.FillWeight = 10;
+                while (num < 2)
+                {
+                    dataGridView1.Columns.Add(checkColumn);
+                    num = num + num;
+                }
+            }
+            catch (Exception)
+            {
+                this.Close();
+                MessageBox.Show("Please select a list", "ERROR");
+            }
+        }
+        //call this when refresh grid after Adding, Editing, or removing data
+        private void refresh_grid()
+        {
+            try
+            {
+                string listName = HomePage.instance.listName.Text;
+                dataGridView1.Refresh();
+                connectionString.Open();
+                SQLiteCommand cmd = new SQLiteCommand($@"SELECT taskName AS ""Name"", dueDate AS ""Due Date"", priority AS ""Priority"" FROM {listName};", connectionString);
+                DataTable dt = new DataTable();
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+               
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+                connectionString.Close();
+
+                DataGridViewCheckBoxColumn checkColumn = new DataGridViewCheckBoxColumn();
+                checkColumn.Name = "X";
+                checkColumn.HeaderText = "X";
+                checkColumn.Width = 50;
+                checkColumn.ReadOnly = false;
+                checkColumn.FillWeight = 10;
+                while (num < 2)
+                {
+                    dataGridView1.Columns.Add(checkColumn);
+                    num = num + num;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
