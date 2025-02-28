992c757480c0c3aacc96339c5dec2540201fd555
Canyon R <Leggomyfneggo@gmail.com>
2025-02-27 20:42:43 -0500

    hopefully this works

--- IT488_CheckMates_CombinedList/IT488_CheckMates_Checklist/TaskPage.Designer.cs
+++ IT488_CheckMates_CombinedList/IT488_CheckMates_Checklist/TaskPage.Designer.cs
@@ -30,12 +30,12 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TaskPage));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.taskGrid = new System.Windows.Forms.DataGridView();
             this.addTask = new System.Windows.Forms.Button();
             this.deleteTasks = new System.Windows.Forms.Button();
             this.listBox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.taskGrid)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -48,21 +48,21 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // dataGridView1
+            // taskGrid
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnF2;
-            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.dataGridView1.Location = new System.Drawing.Point(39, 203);
-            this.dataGridView1.MultiSelect = false;
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ShowCellToolTips = false;
-            this.dataGridView1.ShowEditingIcon = false;
-            this.dataGridView1.Size = new System.Drawing.Size(396, 218);
-            this.dataGridView1.TabIndex = 8;
+            this.taskGrid.AllowUserToAddRows = false;
+            this.taskGrid.AllowUserToDeleteRows = false;
+            this.taskGrid.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.taskGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.taskGrid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnF2;
+            this.taskGrid.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.taskGrid.Location = new System.Drawing.Point(39, 203);
+            this.taskGrid.MultiSelect = false;
+            this.taskGrid.Name = "taskGrid";
+            this.taskGrid.ShowCellToolTips = false;
+            this.taskGrid.ShowEditingIcon = false;
+            this.taskGrid.Size = new System.Drawing.Size(396, 218);
+            this.taskGrid.TabIndex = 8;
             // 
             // addTask
             // 
@@ -108,14 +108,14 @@
             this.Controls.Add(this.listBox);
             this.Controls.Add(this.deleteTasks);
             this.Controls.Add(this.addTask);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.taskGrid);
             this.Controls.Add(this.pictureBox1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "TaskPage";
             this.Text = "CheckItOff";
             this.Load += new System.EventHandler(this.Form1_Load_1);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.taskGrid)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,7 +124,7 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView taskGrid;
         private System.Windows.Forms.Button addTask;
         private System.Windows.Forms.Button deleteTasks;
         private System.Windows.Forms.TextBox listBox;
